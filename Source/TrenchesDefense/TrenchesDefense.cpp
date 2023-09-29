12d14f78ff71a170d07a58612160599bb483f1dc
Flo <florent_cazin@ens.univ-artois.fr>
2023-09-29 15:16:59 +0200

    Project Initialisation
    First push of the blank project

---
+++ Source/TrenchesDefense/TrenchesDefense.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TrenchesDefense.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TrenchesDefense, "TrenchesDefense" );
