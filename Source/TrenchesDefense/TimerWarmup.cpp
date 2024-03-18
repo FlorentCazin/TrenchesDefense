907d42fbc471a7d0513063eb907b283068653dc3
Flo <florent_cazin@ens.univ-artois.fr>
2024-03-18 10:33:35 +0100

    timer in progress
    working, just have to add the gamelogic with the gamemode ...

---
+++ Source/TrenchesDefense/TimerWarmup.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TimerWarmup.h"
+#include "TimerManager.h"
+#include "Engine/World.h"
+
+// Sets default values
+ATimerWarmup::ATimerWarmup()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	TimerText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("TimerTextComponent"));
+}
+
+// Called when the game starts or when spawned
+void ATimerWarmup::BeginPlay()
+{
+	Super::BeginPlay();
+	ActualTime = TimeInSeconds;
+}
+
+// Called every frame
+void ATimerWarmup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATimerWarmup::StartTimer() {
+	GetWorldTimerManager().SetTimer(TimerHandle, this, &ATimerWarmup::ChangeText, 1.f, true, 1.0f);
+}
+
+void ATimerWarmup::ChangeText() {
+	if (ActualTime <= 0) {
+		//get world subsystem et start wave
+		GetWorldTimerManager().ClearTimer(TimerHandle);
+		ActualTime = TimeInSeconds;
+	}
+	else {
+		FString TimerTmp = FString::Printf(TEXT("00:00"));
+		int minutes = ActualTime / 60;
+		int seconds = ActualTime - (minutes * 60);
+		if (minutes == 0 && seconds == 0) {
+			//do nothing, already initialized
+		}
+		else if (minutes < 10) {
+			if (seconds < 10) {
+				TimerTmp = FString::Printf(TEXT("0%d:0%d"), minutes, seconds);
+			}
+			else {
+				TimerTmp = FString::Printf(TEXT("0%d:%d"), minutes, seconds);
+			}
+		}
+		else if (seconds < 10) {
+			if (minutes < 10) {
+				TimerTmp = FString::Printf(TEXT("0%d:0%d"), minutes, seconds);
+			}
+			else {
+				TimerTmp = FString::Printf(TEXT("%d:0%d"), minutes, seconds);
+			}
+		}
+		else {
+			TimerTmp = FString::Printf(TEXT("%d:%d"), minutes, seconds);
+		}
+		TimerText->SetText(FText::FromString(TimerTmp));
+		ActualTime--;
+	}
+}
---
+++ Source/TrenchesDefense/TimerWarmup.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TextRenderComponent.h"
+#include "TimerWarmup.generated.h"
+
+
+UCLASS()
+class TRENCHESDEFENSE_API ATimerWarmup : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATimerWarmup();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "timer warmup")
+	UTextRenderComponent* TimerText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "timer warmup")
+	int TimeInSeconds;
+
+	int ActualTime;
+
+	FTimerHandle TimerHandle;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	void StartTimer();
+
+	void ChangeText();
+
+};
